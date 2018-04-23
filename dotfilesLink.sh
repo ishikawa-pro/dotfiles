4eb32910c4aabc7ffcde201c0fe81caa097477da
ishikawa-pro <ishikawa.pro@gmail.comm>
2018-04-23 20:54:02 +0900

    Add tmux and gitignore symbolic links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mkdir -p ~.vim/bundle
+mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
