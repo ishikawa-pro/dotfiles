c164d71ef57b6381837f6051d35879cf684fd6a1
ishikawa-pro <ishikawa.pro@gmail.comm>
2018-04-24 19:22:54 +0900

    add powerline config file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.config/powerline/
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/powerline ~/.config/powerline
